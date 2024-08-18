1a56034d9a9ab9d156f7eb53358c76e16ea5580f
sid14-14 <siddharthahadimani@gmail.com>
2024-08-19 00:11:04 +0530

    added signup and login ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,16 @@ import Navbar from "./components/Navbar/Navbar";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import Footer from "./components/Footer/Footer";
+import { useState } from "react";
+import LoginPopup from "./components/LoginPopup/LoginPopup";
 
 const App = () => {
+  const [showLogin, setShowLogin] = useState(false);
   return (
     <>
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-        <Navbar />
+        <Navbar setShowLogin={setShowLogin}/>
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
