4a8ff0522ead339b6e1975e666ab41a2be0a2d26
sid14-14 <sidd.work.mail@gmail.com>
2024-08-20 14:12:27 +0530

    wrote code to create an order and successfully able to hit api req to create an order

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { Route, Routes } from "react-router-dom";
+import Add from "./pages/Add/Add";
+import List from "./pages/List/List";
+import Orders from "./pages/Orders/Orders";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
@@ -14,6 +18,11 @@ const App = () => {
         <hr />
         <div className="app-content">
           <Sidebar />
+          <Routes>
+            <Route path="/add" element={<Add url={url} />} />
+            <Route path="/list" element={<List url={url} />} />
+            <Route path="/orders" element={<Orders url={url} />} />
+          </Routes>
         </div>
       </div>
     </>
